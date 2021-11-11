44709884d65b8d07b5adcb084907e13768733405
Aleksei211 <85795990+Aleksei211@users.noreply.github.com>
2021-11-11 08:07:49 +0200

    corrects task5

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,12 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
-nameInput.addEventListener('input', (event => {
-    if (event === '') {
-        nameOutput.textContent = 'Anonymous';
-    }
-        nameOutput.textContent = event.currentTarget.value;
-
-}))
+nameInput.addEventListener('input', onInputChange)
+   function onInputChange(event) {
+       if (nameInput.value === '') {
+           nameOutput.textContent = 'Anonymous';
+       } else {
+           nameOutput.textContent = event.target.value;
+       }
+}
 
